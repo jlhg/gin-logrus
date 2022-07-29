4cc93b2eede5cacf8429fad2582b0931aa54a29a
Jian-Long Huang <contact@jianlong.org>
2022-07-30 02:36:40 +0800

    Update logger fields

--- logger.go
+++ logger.go
@@ -1,9 +1,6 @@
 package ginlogrus
 
 import (
-	"fmt"
-	"math"
-	"net/http"
 	"os"
 	"time"
 
@@ -11,13 +8,8 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// 2016-09-27 09:38:21.541541811 +0200 CEST
-// 127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700]
-// "GET /apache_pb.gif HTTP/1.0" 200 2326
-// "http://www.example.com/start.html"
-// "Mozilla/4.08 [en] (Win98; I ;Nav)"
-
-var timeFormat = "02/Jan/2006:15:04:05 -0700"
+// TimeFormat represents time format layout.
+var TimeFormat = time.RFC3339
 
 // Logger is the logrus logger handler
 func Logger(logger logrus.FieldLogger, notLogged ...string) gin.HandlerFunc {
@@ -37,12 +29,15 @@ func Logger(logger logrus.FieldLogger, notLogged ...string) gin.HandlerFunc {
 	}
 
 	return func(c *gin.Context) {
-		// other handler can change c.Path so:
 		path := c.Request.URL.Path
+		rawQuery := c.Request.URL.RawQuery
 		start := time.Now()
+
+		// Process request
 		c.Next()
-		stop := time.Since(start)
-		latency := int(math.Ceil(float64(stop.Nanoseconds()) / 1000000.0))
+
+		stop := time.Now()
+		latency := stop.Sub(start)
 		statusCode := c.Writer.Status()
 		clientIP := c.ClientIP()
 		clientUserAgent := c.Request.UserAgent()
@@ -59,26 +54,18 @@ func Logger(logger logrus.FieldLogger, notLogged ...string) gin.HandlerFunc {
 		entry := logger.WithFields(logrus.Fields{
 			"hostname":   hostname,
 			"statusCode": statusCode,
-			"latency":    latency, // time to process
+			"latency":    latency,
 			"clientIP":   clientIP,
 			"method":     c.Request.Method,
 			"path":       path,
+			"rawQuery":   rawQuery,
 			"referer":    referer,
 			"dataLength": dataLength,
 			"userAgent":  clientUserAgent,
+			"time":       start.Format(TimeFormat),
+			"error":      c.Errors.ByType(gin.ErrorTypePrivate).String(),
 		})
 
-		if len(c.Errors) > 0 {
-			entry.Error(c.Errors.ByType(gin.ErrorTypePrivate).String())
-		} else {
-			msg := fmt.Sprintf("%s - %s [%s] \"%s %s\" %d %d \"%s\" \"%s\" (%dms)", clientIP, hostname, time.Now().Format(timeFormat), c.Request.Method, path, statusCode, dataLength, referer, clientUserAgent, latency)
-			if statusCode >= http.StatusInternalServerError {
-				entry.Error(msg)
-			} else if statusCode >= http.StatusBadRequest {
-				entry.Warn(msg)
-			} else {
-				entry.Info(msg)
-			}
-		}
+		entry.Info()
 	}
 }
